18153e28e10e2253afb9f9ad8bf9924623ce903b
Mustafa Alhamadani <53314204+MustafaAlhamadani@users.noreply.github.com>
2020-09-06 15:06:12 +0300

    Update Covid-19 continent figures.py

--- covid-19 figure/Covid-19 continent figures.py
+++ covid-19 figure/Covid-19 continent figures.py
@@ -30,19 +30,19 @@ fig, (ax1, ax2, ax3) = plt.subplots(nrows=3, ncols=1, figsize=(20, 20))
 
 
 plot = ax1.bar(data['WHO Region'], data['Confirmed'])
-ax1.set(xlabel='Continents', ylabel='Confirmed Cases (in millions)')
+ax1.set(xlabel='Regions', ylabel='Confirmed Cases (in millions)')
 font = {'weight' : 'bold',
         'size'   : 20}
 plt.rc('font', **font)
 
 
 plot2 = ax2.bar(data['WHO Region'], data['Deaths'])
-ax2.set(xlabel='Continents', ylabel='Death Cases', )
+ax2.set(xlabel='Regions', ylabel='Death Cases', )
 
 
 plot3 = ax3.bar(data['WHO Region'], data['Recovered']);
-ax3.set(xlabel= 'Continents', ylabel='Recovered cases (in millions)')
-fig.suptitle('COVID-19 Continents comparison (2020/08)');
+ax3.set(xlabel= 'Regions', ylabel='Recovered cases (in millions)')
+fig.suptitle('COVID-19 Regions comparison (2020/08)');
 
 
 # In[240]:
@@ -68,7 +68,7 @@ confirmed
 
 fig, ax = plt.subplots(figsize=(10,10))
 plot = ax.bar(data['WHO Region'], data['Confirmed'])
-ax.set(xlabel='Continents', ylabel='Confirmed Cases (in millions)', title='COVID-19 Confirmed Cases')
+ax.set(xlabel='Regions', ylabel='Confirmed Cases (in millions)', title='COVID-19 Confirmed Cases')
 font = {'weight' : 'bold',
         'size'   : 20}
 plt.rc('font', **font)
@@ -77,7 +77,7 @@ plt.rc('font', **font)
 # In[19]:
 
 
-fig.savefig('covid confirmed.png')
+fig.savefig('covid-19 confirmed.png')
 
 
 # In[24]:
@@ -85,13 +85,13 @@ fig.savefig('covid confirmed.png')
 
 fig, ax = plt.subplots(figsize=(10,10))
 plot = ax.bar(data['WHO Region'], data['Deaths'])
-ax.set(xlabel='Continents', ylabel='Death Cases',title='Covid-19 Death Cases' )
+ax.set(xlabel='Regions', ylabel='Death Cases',title='Covid-19 Death Cases' )
 
 
 # In[25]:
 
 
-fig.savefig('covid deaths.png')
+fig.savefig('covid-19 deaths.png')
 
 
 # In[26]:
@@ -99,13 +99,13 @@ fig.savefig('covid deaths.png')
 
 fig, ax = plt.subplots(figsize=(10,10))
 plot = ax.bar(data['WHO Region'], data['Recovered']);
-ax.set(xlabel= 'Continents', ylabel='Recovered cases (in millions)', title='COVID-19 Recovered cases')
+ax.set(xlabel= 'Regions', ylabel='Recovered cases (in millions)', title='COVID-19 Recovered cases')
 
 
 # In[23]:
 
 
-fig.savefig('Covid Recovered')
+fig.savefig('Covid-19 Recovered.png')
 
 
 # In[50]:
@@ -113,13 +113,13 @@ fig.savefig('Covid Recovered')
 
 fig, ax = plt.subplots(figsize=(10,10))
 plt.barh(data['WHO Region'],data['Deaths / 100 Cases'])
-ax.set(xlabel='Deaths per 100 cases', ylabel='Continents',title='Covid-19 Deaths per 100 cases' );
+ax.set(xlabel='Deaths per 100 cases', ylabel='Regions',title='Covid-19 Deaths per 100 cases' );
 
 
 # In[51]:
 
 
-fig.savefig('deaths per 100 cases.png')
+fig.savefig('covid-19 deaths per 100 cases.png')
 
 
 # In[53]:
@@ -127,13 +127,13 @@ fig.savefig('deaths per 100 cases.png')
 
 fig, ax = plt.subplots(figsize=(15,10))
 plt.barh(data['WHO Region'],data['Recovered / 100 Cases'])
-ax.set(xlabel='Recovered per 100 cases', ylabel='Continents',title='Covid-19 Recoveries per 100 cases' );
+ax.set(xlabel='Recovered per 100 cases', ylabel='Regions',title='Covid-19 Recoveries per 100 cases' );
 
 
 # In[54]:
 
 
-fig.savefig('Recovered per 100 cases.png')
+fig.savefig('covid-19 Recovered per 100 cases.png')
 
 
 # In[ ]:
